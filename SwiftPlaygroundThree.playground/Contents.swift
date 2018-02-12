58f214ddb3d70300bfba99df031186e5797eaab4
ScottUHWO <salquisa@hawaii.edu>
2018-02-12 10:16:55 -1000

    -how do we declare a variable -Simple string variable -Initalize an array using an Array literal -for-in loop -let's print out item at Index 1

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,35 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//how do we declare a variable
+
+var fruitBasket: [String]
+
+//Simple string variable
+
+var simpleString: String
+
+//Initalize an array using an Array literal
+
+fruitBasket = ["Apples", "Pineapple", "Oranges", "Watermelon"]
+
+//print fruitBasket
+
+print(fruitBasket)
+
+//for-in loop
+
+for name in fruitBasket{
+//print "I like to eat" before every name in fruitBasket
+    print("I like to eat " + name)
+}
+
+//let's print out item at Index 1
+
+print(fruitBasket[1])
+print(fruitBasket[0])
+
+
+
+
